e412656a7f14e1de365eaf7c0397c09517ec5949
lngr <60884886+louangr@users.noreply.github.com>
2023-04-23 00:28:36 +0200

    add graphic part of account management edition

--- Translator/Translator/Views/SettingsPageViewModel.cs
+++ Translator/Translator/Views/SettingsPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
 using Translator.Core;
 
 namespace Translator.Views
@@ -8,15 +10,66 @@ namespace Translator.Views
 
         #region Private fields
 
+        private string apiKeyInput;
+        private string accountNameInput;
+        private ObservableCollection<Tuple<string, string>> accounts;
         private RelayCommand goBackCommand;
+        private RelayCommand addAccountCommand;
 
         #endregion Private fields
 
         #region Properties
 
+        public string ApiKeyInput
+        {
+            get => apiKeyInput;
+            set
+            {
+                apiKeyInput = value;
+                OnPropertyChanged(nameof(ApiKeyInput));
+                AddAccountCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        public string AccountNameInput
+        {
+            get => accountNameInput;
+            set
+            {
+                accountNameInput = value;
+                OnPropertyChanged(nameof(AccountNameInput));
+                AddAccountCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        public ObservableCollection<Tuple<string, string>> Accounts
+        {
+            get => accounts;
+            set
+            {
+                accounts = value;
+                OnPropertyChanged(nameof(Accounts));
+            }
+        }
+
+        public bool IsAddAccountButtonEnabled => ApiKeyInput != null && ApiKeyInput.Trim() != string.Empty && AccountNameInput != null && AccountNameInput.Trim() != string.Empty;
+
+        public bool IsAccountsListViewVisible => Accounts != null && Accounts.Count > 0;
+
         public RelayCommand GoBackCommand
             => goBackCommand ?? (goBackCommand = new RelayCommand(() => GoBack()));
 
+        public RelayCommand AddAccountCommand
+            => addAccountCommand ?? (addAccountCommand = new RelayCommand(() => AddAcount(), () => IsAddAccountButtonEnabled));
+
         #endregion Properties
+
+        #region Private methods
+
+        private void AddAcount()
+        {
+        }
+
+        #endregion Private methods
     }
 }
